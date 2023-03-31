bdcbcbfef7e12d45ced8389fd48853f98163147f
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-31 16:23:47 +0300

    deleted razor pages support moved, app.run in program, and deleted unnecesary dependencies

--- Back-End/VaraticPrim/VaraticPrim/Program.cs
+++ Back-End/VaraticPrim/VaraticPrim/Program.cs
@@ -2,6 +2,12 @@ using VaraticPrim;
 
 var builder = WebApplication.CreateBuilder(args);
 var startup = new Startup(builder.Configuration);
+
 startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+
 var app = builder.Build();
+
 startup.Configure(app, builder.Environment); // calling Configure method
+app.MapControllers();
+
+app.Run();
