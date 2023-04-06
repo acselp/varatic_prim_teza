60cfa8e5b36eec2004e37857972af72bddd2cb58
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-04-06 20:15:04 +0300

    renamed custom validator extensions class

--- Back-End/VaraticPrim/VaraticPrim/Validation/CustomValidatorExtensions.cs
+++ Back-End/VaraticPrim/VaraticPrim/Validation/CustomValidatorExtensions.cs
@@ -3,7 +3,7 @@ using FluentValidation;
 
 namespace VaraticPrim.Validation;
 
-public static class CustomPhoneValidatorExtension
+public static class CustomValidatorExtensions
 {
     public static IRuleBuilderOptions<T, string> IsMoldovaMobile<T>(this IRuleBuilder<T, string> ruleBuilder)
     {
